2a33ecc6482fd6ed8d49c60e2f7ce8549e6dda0c
Charlie Migwi <charliemigwi@gmail.com>
2019-07-26 15:17:24 +0300

    add syntax to toggle we-do section

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,17 @@
 //Code to toggle we-do
 $(function(){
-  $(".item1").click(function(){
+  $(".item-1").click(function(){
     $("#services1").toggle();
-    $("#desc1").toggle();
+    $("#des1").toggle();
   });
-  $(".item2").click(function(){
+  $(".item-2").click(function(){
     $("#services2").toggle();
-    $("#desc2").toggle();
+    $("#des2").toggle();
   });
-  $(".item3").click(function(){
+  $(".item-3").click(function(){
     $("#services3").toggle();
-    $("#desc3").toggle();
+    $("#des3").toggle();
   });
 });
+
+// For hover effect on PORTFOLIO
